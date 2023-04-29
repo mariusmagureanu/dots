79324949ca6fa860097e6c9be94398e5be687296
Marius Magureanu <marius.magureanu@schibsted.com>
2023-04-29 21:29:09 +0200

    Added nvim config and plugins

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+vim.opt.termguicolors = true
+vim.opt.completeopt = "menu"
+
+require("plugins-setup")
+
+require("core.colorscheme")
+require("core.options")
+require("core.keymap")
+
+require("plugins.nvim-tree")
+require("plugins.telescope")
+require("plugins.gitsigns")
+require("plugins.comment")
+require("plugins.treesitter")
+require("plugins.autopairs")
+require("plugins.lualine")
+require("plugins.nvim-cmp")
+
+require("plugins.lsp.mason")
+require("plugins.lsp.lspsaga")
+require("plugins.lsp.lspconfig")
+require("plugins.lsp.null-ls")
---
+++ nvim/lua/plugins/lsp/lspsaga.lua
@@ -0,0 +1,61 @@
+-- import lspsaga safely
+local saga_status, _ = pcall(require, "lspsaga")
+if not saga_status then
+	print("could not import lspsaga")
+	return
+end
+
+local keymap = vim.keymap.set
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+-- Code action
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename
+keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+-- Rename word in whole project
+keymap("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+
+-- Peek Definition
+-- you can edit the definition file in this float window
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+
+-- Go to Definition
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Show cursor diagnostic
+-- also like show_line_diagnostics  support pass ++unfocus
+keymap("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostic
+keymap("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+-- Toggle Outline
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Callhierarchy
+keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+-- Float terminal
+keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+
+-- saga.init_lsp_saga({
+-- 	-- keybinds for navigation in lspsaga window
+-- 	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+-- 	-- use enter to open file with finder
+-- 	finder_action_keys = {
+-- 		open = "<CR>",
+-- 	},
+-- 	-- use enter to open file with definition preview
+-- 	definition_action_keys = {
+-- 		edit = "<CR>",
+-- 	},
+-- })
