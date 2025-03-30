77a1949cb0fc97dd53ecfa6b4cd2103122db5bed
Marius Magureanu <mariusmagureanu@live.dk>
2025-03-30 13:50:39 +0200

    Apply stylua across the board

--- nvim-lua/lua/plugins/null-ls.lua
+++ nvim-lua/lua/plugins/null-ls.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    lazy = false,
-    config = function()
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	{
+		"nvimtools/none-ls.nvim",
+		lazy = false,
+		config = function()
+			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      null_ls.setup({
+			null_ls.setup({
 
-        sources = {
-          formatting.stylua, -- lua formatter
-          formatting.gofmt, -- go formatter
-          formatting.black,
-          formatting.prettier,
-          formatting.yamlfmt,
-          formatting.shfmt,
-          formatting.terraform_fmt,
-        },
-      })
-    end,
-  },
+				sources = {
+					formatting.stylua, -- lua formatter
+					formatting.gofmt, -- go formatter
+					formatting.black,
+					formatting.prettier,
+					formatting.yamlfmt,
+					formatting.shfmt,
+					formatting.terraform_fmt,
+				},
+			})
+		end,
+	},
 }
